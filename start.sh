99f1bb6a6bcb65ac45ab90d9f629857e93332ef3
Dxy <denight@qq.com>
2017-07-19 09:45:09 +0800

    调整命令

--- start.sh
+++ start.sh
@@ -2,9 +2,10 @@
 
 networkservice=$(networksetup -listallnetworkservices | head -n 3 | tail -n 1)  #获取Mac当前使用的网络名称
 
-echo Open web proxy for networkservice: $networkservice
 if [ $1 == "on" ]
     then
+        echo Open web proxy for networkservice: $networkservice
+    
         sudo networksetup -setwebproxy "$networkservice" $2 $3   #设置Web HTTP代理
         # sudo networksetup -setsecurewebproxy "$networkservice" $2 $3   #设置Web HTTPS代理
         sudo networksetup -setwebproxystate "$networkservice" on   #打开Web HTTP代理
@@ -14,6 +15,8 @@ if [ $1 == "on" ]
 
         forever -w index.js -watchDirectory ./
     else
+        echo Close web proxy for networkservice: $networkservice
+    
         sudo networksetup -setwebproxystate "$networkservice" off   #打开Web HTTP代理
         # sudo networksetup -setsecurewebproxystate "$networkservice" off  #打开Web HTTPS代理
 
